7dadb6bfac476ff93c525b299f0a5b5b15e032be
Saiyoran1 <alecsimpson12345@gmail.com>
2023-02-08 19:43:21 -0500

    Posting builds
    - Worked on getting posting of builds working.
    - Added logic for simplifying builds before saving to the backend.
    -  Ran into some issues with CORS preventing either content-type of JSON or POST requests, not sure if both or just one was the issue.
    - req.body is undefined on the backend.

--- src/Generic/FrontEndDataFetching.js
+++ src/Generic/FrontEndDataFetching.js
@@ -21,3 +21,54 @@ export function fetchDataForSpec(specID, cb) {
         });
     }
 }
+
+const isDefaultBuild = (build) => {
+    if (!build || Object.keys(build).length === 0) {
+        return true;
+    }
+    if (isNaN(build.spec.id)) {
+        return true;
+    }
+    return false;
+}
+
+const saveAncientBuild = (ancientBuild) => {
+    return {
+        spec: ancientBuild.spec.id,
+        talents: ancientBuild.talentRows.map(row => [row.base.id, row.selection === -1 ? row.base.id : row.talents[row.selection].id])
+    }
+}
+
+const saveModernBuild = (modernBuild) => {
+    return {
+        spec: modernBuild.spec.id,
+        abilities: Array.from(modernBuild.abilities.free.keys()).map(key => modernBuild.abilities.free.get(key).id)
+    }
+}
+
+export function saveBuild(buildName, buildDescription, ancientBuild, modernBuild) {
+    if (!buildName || buildName === "" || !buildDescription || buildDescription === "") {
+        return;
+    }
+    if (isDefaultBuild(ancientBuild) || isDefaultBuild(modernBuild)) {
+        return;
+    }
+    const requestOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: buildName,
+            description: buildDescription,
+            ancientBuild: saveAncientBuild(ancientBuild),
+            modernBuild: saveModernBuild(modernBuild)
+        })
+    }
+    fetch(`http://localhost:5000/build/`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
--- src/Generic/SaveBuildForm.js
+++ src/Generic/SaveBuildForm.js
@@ -1,19 +1,22 @@
 import styles from '../styles/SaveBuildForm.module.css';
 import {useState} from 'react';
+import {saveBuild} from './FrontEndDataFetching';
 
 const SaveBuildForm = ({ancientBuild, modernBuild}) => {
 
     const [buildName, setBuildName] = useState("");
+    const [buildDescription, setBuildDescription] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log([ancientBuild, modernBuild, buildName]);
+        saveBuild(buildName, buildDescription, ancientBuild, modernBuild);
     }
     
     return (
         <form className={styles["save-form"]} onSubmit={handleSubmit}>
             <label htmlFor="buildName">Build Name:</label>
             <input type="text" id="buildName" name="buildName" value={buildName} onChange={e => setBuildName(e.target.value)}/>
+            <input type="textarea" id="buildDescription" name="buildDescription" value={buildDescription} onChange={e => setBuildDescription(e.target.value)}/>
             <button type="submit" className={styles["save-button"]}>Save Build</button>
         </form>
     )
@@ -25,6 +28,7 @@ export default SaveBuildForm;
 Saved build shape:
 {
     name: string,
+    description: string,
     ancientBuild: {
         specId: number,
         talents: Map(number, number) //this maps base id and talent id
